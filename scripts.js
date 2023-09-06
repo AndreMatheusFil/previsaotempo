d0ae837c897b0898a1c8c69cf18a924b64372e65
André M. Fil <andrematheusfil@gmail.com>
2023-09-06 02:04:48 -0300

    Escondendo a key da api

--- scripts.js
+++ scripts.js
@@ -1,4 +1,3 @@
-const key = "6ca388b417042fecbaf07bb173ecd379";
 const inputEle = document.querySelector(".in-city");
 
 function plotaDados(dados){
@@ -10,20 +9,23 @@ function plotaDados(dados){
     console.log(document.querySelector(".img-temp").scr);
 }
 
-async function bucarCidade(cidade){
+async function bucarCidade(cidade, key){
     const dados = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${key}&lang=pt_br&units=metric`).then( resposta => resposta.json());
     console.log(dados);
     plotaDados(dados);
 }
 
-function clique() {
+async function clique() {
     const input = document.querySelector(".in-city").value;
-    console.log(input);
-    bucarCidade(input);
+    key = await fetch("./variavel.json").then( resposta => resposta.json());
+    key = key.key
+    console.log(key);
+    bucarCidade(input, key);
 }
 
 document.addEventListener("keyup",function(e){
     if (e.key == 'Enter') {
+        console.log(key)
          clique()
     }
 }, false);  
